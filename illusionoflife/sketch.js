87994fbf4672fa159260ea782c3412b9580e25e6
Vinnie Park <parkvinnie@gmail.com>
2023-09-21 10:43:59 -0400

    added comments

--- illusionoflife/sketch.js
+++ illusionoflife/sketch.js
@@ -1,11 +1,11 @@
-let boxHeight = 20; 
-let boxWidth = 20; 
-let xPos = 200; 
-let yPos = 400; 
-let isSquishing = false; 
-let isUnsquish = false; 
-let isJumping = false; 
-let isEnding = false; 
+let boxHeight = 20; //height var 
+let boxWidth = 20; //Width var 
+let xPos = 200; //X Positioning
+let yPos = 400; //Y Positioning 
+let isSquishing = false; //Flag for compressing box
+let isUnsquish = false; //Flag for decompressing box 
+let isJumping = false; //Flag for while jumping 
+let isEnding = false; //Flag for landing
 
 //JUMPING PHYSICS DEPENDENCIES
 let dy = 0; 
@@ -14,60 +14,57 @@ let dx = 0;
 function setup() {
   createCanvas(400, 400);
   rectMode(CORNERS);
-  //ddx = random(5);
-  //ddy = 2;
 }
 
 function draw() {
   background(220);
   rect(xPos-(boxWidth/2),yPos-boxHeight,xPos+(boxWidth/2),yPos);
-  if(isSquishing){
+  if(isSquishing){ //While squishing change height and width 
     dy = 10; 
-    //dx = 1.5; 
     if(boxHeight > 6){
       boxHeight -= 2; 
     }
     if(boxWidth < 34){
       boxWidth += 2; 
     }
-    else{
+    else{ //When process ends set off a new flag
       isSquishing = false; 
       isUnsquish = true; 
       isJumping = false; 
       isEnding = false;
     }
   }
-  if(isUnsquish){
+  if(isUnsquish){ //Undo the actions in squish and elongate the box
     if(boxHeight < 36){
       boxHeight += 10; 
     }
     if(boxWidth > 20){
       boxWidth -= 5; 
     }
-    else{
+    else{ //Move to the jumping stage 
       isSquishing = false; 
       isUnsquish = false; 
       isJumping = true; 
       isEnding = false;
     }
   }
-  if(isJumping){
+  if(isJumping){ //Use physics to move box up like a jump and simulate gravity
     console.log(yPos);
     yPos -= dy; 
     xPos += dx; 
     dy -= 0.2;
-    if(round(yPos) == 400){
+    if(round(yPos) == 400){ //If the box hits ground move to next step 
       isEnding = true; 
       isSquishing = false;
       isUnsquish = false; 
       isJumping = false;
     }
   }
-  if(isEnding){
+  if(isEnding){ //Unelongate the box so it moves back to its natural square shape 
     if(boxHeight != 20){
       boxHeight -= 4;
     }
-    else{
+    else{ //End the flag loop and wait until mouse is pressed
       isEnding = false; 
       isSquishing = false;
       isUnsquish = false; 
@@ -77,8 +74,8 @@ function draw() {
 }
 
 function mousePressed(){
-  if(isEnding == false && isSquishing == false && isUnsquish == false && isJumping == false){
+  if(isEnding == false && isSquishing == false && isUnsquish == false && isJumping == false){ //If box is not in process start the loop
     isSquishing = true; 
-    dx = (mouseX-xPos)/100;
+    dx = (mouseX-xPos)/100; //How far the box will move to the left or right 
   }
 }
