0b8b6a40e0dc1b98832e919364489f595d019c66
Vinnie Park <parkvinnie@gmail.com>
2023-12-07 04:22:09 -0500

    pivoted, made a smash bros type game

--- _FINALPROJECTBASICCODE/sketch.js
+++ _FINALPROJECTBASICCODE/sketch.js
@@ -1,91 +1,142 @@
-let backingSong;
-let startButton; 
-let restartButton; 
-let camCapture;
-let progress; 
-let gameBeingPlayed;
-
-function preload() {
-  soundFormats('mp3', 'ogg');
-  backingSong = loadSound('assets/Rasputin');
-}
+let player1;
+let player2; 
+let keysPressed = {};
+let keysTimeStamps = {}; 
 
 function setup() {
-  // Create Canvas
-  createCanvas(600, 600);
-  // Button Set Up 
-  startButton = createButton("Play Level");
-  startButton.position(250,250);
-  startButton.mousePressed(PlaySong);
-
-  restartButton = createButton("Restart Level");
-  restartButton.position(500,20);
-  restartButton.mousePressed(RestartSong);
-  restartButton.hide();
-  // Webcam
-  camCapture = createCapture(VIDEO);
-  camCapture.hide(); 
-  background(0);
-  gameBeingPlayed = false; 
-  // add second to progress
-  setInterval(IncrementProgress,4000);
+  player1 = new Player1(300,300); 
+  player2 = new BingBing(500,300);
+  createCanvas(900,500);
 }
 
 function draw() {
-  if (gameBeingPlayed){
-    // Doing what you gotta do
-    background(0);
-    // Draw webcam
-    image(camCapture,0,0);
-    // Draw 5 empty stars in top corner 
-    noFill();
-    strokeWeight(4);
-    stroke(color(252, 186, 3));
-    circle(30,70,40);
-    circle(80,70,40);
-    circle(130,70,40);
-    circle(180,70,40);
-    circle(230,70,40);
-    strokeWeight(1);
-    textSize(30);
-    text("Score",20,35);
-    // Draw Progress Bar
-    strokeWeight(10);
-    stroke(color(87, 167, 207))
-    line(0,480,progress,480);
-    // Draw Spotlight
-    noStroke();
-    fill(255, 238, 168)
-    beginShape();
-    vertex(250,600);
-    vertex(350,600);
-    vertex(325,480);
-    vertex(275,480);
-    endShape(CLOSE);
-    strokeWeight(15);
-    stroke(255);
-    line(250,600,350,600);
+  background(0);
+  rectMode(CORNERS);
+  fill(255);
+  rect(200,425,700,440);
+  player1.renderPlayer();
+  player2.renderPlayer();
+  player1.movePlayer(); 
+  player2.movePlayer();
+}
+
+class Player1{
+  constructor(xpos,ypos){
+    this.xpos = xpos;
+    this.ypos = ypos; 
+    this.xvelocity = 0;
+    this.yvelocity = 0; 
+    this.touchingGround = false; 
+    this.xPrevPos; 
+    this.yPrevPos;
   }
-  else {
-    background(0);
-    image(camCapture,0,0)
+  renderPlayer(){
+    fill(133, 255, 117);
+    circle(this.xpos,this.ypos,30);
+  }
+  movePlayer(){
+    if(this.ypos >= 400 && this.xpos < 700 && this.xpos > 200 && this.yPrevPos <= 400){
+      this.ypos = 400; 
+      this.touchingGround = true; 
+    }
+    if(keysPressed['d'] && keysPressed['e']){
+      fill(255)
+      rect(this.xpos - 15,this.ypos + 5, this.xpos, this.ypos - 5);
+      triangle(this.xpos,this.ypos - 15,this.xpos + 15, this.ypos, this.xpos,this.ypos + 15);
+      if (dist(this.xpos,this.ypos,player2.xpos,player2.ypos) <= 30){
+        player2.xvelocity = 10; 
+      }
+    }
+    else if(keysPressed['d']){
+      console.log(this.xvelocity);
+      this.xvelocity = 5; 
+    }
+    if(keysPressed['a'] && keysPressed['e']){
+      fill(255)
+      rect(this.xpos + 15,this.ypos - 5, this.xpos, this.ypos + 5);
+      triangle(this.xpos,this.ypos + 15,this.xpos - 15, this.ypos, this.xpos,this.ypos - 15);
+      if (dist(this.xpos,this.ypos,player2.xpos,player2.ypos) <= 30){
+        player2.xvelocity = -10; 
+      }
+    }
+    else if(keysPressed['a']){
+      this.xvelocity = -5; 
+    }
+    if (!keysPressed['a'] && !keysPressed['d']){
+      this.xvelocity = 0;
+    }
+    if(keysPressed['w'] && keysPressed['e']){
+      fill(255)
+      rect(this.xpos - 5,this.ypos + 15, this.xpos + 5, this.ypos);
+      triangle(this.xpos - 15,this.ypos,this.xpos, this.ypos - 15, this.xpos + 15,this.ypos);
+      if (dist(this.xpos,this.ypos,player2.xpos,player2.ypos) <= 30){
+        player2.yvelocity = -10; 
+      }
+    }
+    else if(keysPressed['w'] && this.touchingGround == true){
+      this.yvelocity = -10;
+      this.touchingGround = false; 
+    }
+    if(keysPressed['s'] && keysPressed['e']){
+      fill(255)
+      rect(this.xpos + 5,this.ypos - 15, this.xpos - 5, this.ypos);
+      triangle(this.xpos + 15,this.ypos,this.xpos, this.ypos + 15, this.xpos - 15,this.ypos);
+      if (dist(this.xpos,this.ypos,player2.xpos,player2.ypos) <= 30){
+        player2.yvelocity = 10; 
+      }
+    }
+    else if(keysPressed['s'] && this.touchingGround == false){
+      this.yvelocity = 10; 
+    }
+    if(this.ypos > 400) {
+      this.touchingGround = false; 
+    }
+    if(this.touchingGround == false){
+      this.yvelocity += 0.5; 
+    }
+    this.xPrevPos = this.xpos;
+    this.yPrevPos = this.ypos; 
+    this.xpos += this.xvelocity;
+    this.ypos += this.yvelocity; 
   }
 }
 
-function PlaySong() {
-  backingSong.play();
-  gameBeingPlayed = true; 
-  progress = 0; 
-  startButton.hide();
-  restartButton.show();
+class BingBing{
+  constructor(xpos,ypos){
+    this.xpos = xpos;
+    this.ypos = ypos; 
+    this.xvelocity = 0;
+    this.yvelocity = 0; 
+    this.touchingGround = false; 
+    this.xPrevPos; 
+    this.yPrevPos;
+  }
+  renderPlayer(){
+    fill(133, 255, 117);
+    circle(this.xpos,this.ypos,30);
+  }
+  movePlayer(){
+    if(this.ypos >= 400 && this.xpos < 700 && this.xpos > 200 && this.yPrevPos <= 400){
+      this.ypos = 400; 
+      this.touchingGround = true; 
+    }
+    if(this.ypos < 400) {
+      this.touchingGround = false; 
+    }
+    if(this.touchingGround == false){
+      this.yvelocity += 0.5; 
+    }
+    this.xPrevPos = this.xpos;
+    this.yPrevPos = this.ypos; 
+    this.xpos += this.xvelocity;
+    this.ypos += this.yvelocity; 
+  }
 }
 
-function RestartSong() {
-  backingSong.stop();
-  backingSong.play();
-  progress = 0; 
+function keyPressed(){
+  keysPressed[key] = true; 
 }
 
-function IncrementProgress() {
-  progress++; 
+function keyReleased(){
+  keysPressed[key] = false; 
 }
